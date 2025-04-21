5a6e0d48de6d5c840787e4af4f8f86dd866018c7
pear_c <zxcv9676@naver.com>
2025-04-21 22:17:07 +0900

    [feat] Student 를 저장할 ConcurrentHashMap 기반 저장소

--- src/main/java/com/nhnacademy/student/repository/MapStudentRepository.java
+++ src/main/java/com/nhnacademy/student/repository/MapStudentRepository.java
@@ -2,45 +2,46 @@ package com.nhnacademy.student.repository;
 
 import com.nhnacademy.student.entity.Student;
 
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MapStudentRepository implements StudentRepository {
-    Map<String, Student> map = new HashMap<>();
+
+    private Map<String, Student> studentsMap = new ConcurrentHashMap<>();
 
     @Override
     public void save(Student student) {
-        map.put(student.getId(), student);
+        studentsMap.put(student.getId(), student);
     }
 
     @Override
     public void update(Student student) {
-        map.put(student.getId(), student);
+        studentsMap.put(student.getId(), student);
     }
 
     @Override
     public void deleteById(String id) {
-        map.remove(id);
+        studentsMap.remove(id);
     }
 
     @Override
     public Student getStudentById(String id) {
-        return map.get(id);
+        return studentsMap.get(id);
     }
 
     @Override
     public List<Student> getStudents() {
         List<Student> students = new LinkedList<>();
-        for(String id : map.keySet()) {
-            students.add(map.get(id));
+        for(String id : studentsMap.keySet()) {
+            students.add(studentsMap.get(id));
         }
         return students;
     }
 
     @Override
     public boolean existById(String id) {
-        return map.containsKey(id);
+        return studentsMap.containsKey(id);
     }
 }
