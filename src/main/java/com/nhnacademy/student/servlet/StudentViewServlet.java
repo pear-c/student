162a546e4cd04acf6d3134f8d9d26c0d95786fcc
pear_c <zxcv9676@naver.com>
2025-04-22 15:37:08 +0900

    [feat] 학생 상세 정보 조회 용 서블릿 생성

---
+++ src/main/java/com/nhnacademy/student/servlet/StudentViewServlet.java
@@ -0,0 +1,47 @@
+package com.nhnacademy.student.servlet;
+
+import com.nhnacademy.student.entity.Student;
+import com.nhnacademy.student.repository.StudentRepository;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@Slf4j
+@WebServlet(name = "studentViewServlet", urlPatterns = "/student/view")
+public class StudentViewServlet extends HttpServlet {
+
+    private StudentRepository studentRepository;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        studentRepository = (StudentRepository) config.getServletContext().getAttribute("studentRepository");
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+
+        //todo id null check
+        if (Objects.isNull(id) || id.isEmpty()) {
+            throw new IllegalStateException();
+        }
+
+        //todo student 조회
+        Student student = studentRepository.getStudentById(id);
+        req.setAttribute("student", student);
+
+        //todo /student/view.jsp <-- forward
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/WEB-INF/student/view.jsp");
+        rd.forward(req, resp);
+    }
+}
