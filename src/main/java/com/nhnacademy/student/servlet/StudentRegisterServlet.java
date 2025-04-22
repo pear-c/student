9371b5d91e097d81068f185e1d14b05b81848efb
pear_c <zxcv9676@naver.com>
2025-04-22 10:03:07 +0900

    [feat] 학생 등록 용 서블릿 생성 - StudentRegisterServlet.java

---
+++ src/main/java/com/nhnacademy/student/servlet/StudentRegisterServlet.java
@@ -0,0 +1,64 @@
+package com.nhnacademy.student.servlet;
+
+import com.nhnacademy.student.entity.Gender;
+import com.nhnacademy.student.entity.Student;
+import com.nhnacademy.student.repository.StudentRepository;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Slf4j
+@WebServlet(name = "studentRegisterServlet", urlPatterns = "/student/register")
+public class StudentRegisterServlet extends HttpServlet {
+
+    private StudentRepository studentRepository;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        //todo init studentRepository
+        super.init(config);
+        studentRepository = (StudentRepository) config.getServletContext().getAttribute("studentRepository");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //todo  /student/register.jsp forward 합니다.
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/WEB-INF/student/register.jsp");
+        rd.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //todo null check
+        String id = req.getAttribute("id").toString();
+        String name = req.getAttribute("name").toString();
+        Gender gender = (Gender) req.getAttribute("gender");
+        int age = (int) req.getAttribute("age");
+        LocalDateTime createdAt = LocalDateTime.now();
+
+        if(Objects.isNull(id) || Objects.isNull(name) || Objects.isNull(gender) || Objects.isNull(age)) {
+            // 에러 메시지 구현 필요
+            return;
+        }
+        if(id.isEmpty() || name.isEmpty()) {
+            // 에러 메시지 구현 필요
+            return;
+        }
+
+        //todo save 구현
+        Student student = new Student(id, name, gender, age, createdAt);
+        studentRepository.save(student);
+
+        //todo redirect /student/view?id=student1
+        resp.sendRedirect("/student/view?id=student1");
+    }
+}
